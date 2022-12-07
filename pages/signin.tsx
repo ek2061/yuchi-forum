6ec8896d47516113dbb5b5d33c57581017165a9a
ek2061 <jackey860517@gmail.com>
2022-12-07 10:00:41 +0800

    refactor: move Sign In/Up Form to page

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -1,9 +1,53 @@
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import LockIcon from "@mui/icons-material/Lock";
 import Box from "@mui/material/Box";
-import { MainContainer } from "components";
+import Button from "@mui/material/Button";
+import Divider from "@mui/material/Divider";
+import Typography from "@mui/material/Typography";
+import {
+  HorizontalEnd,
+  MainContainer,
+  RegisterFormContainer,
+  TextInput,
+} from "components";
 import type { NextPage } from "next";
 import Head from "next/head";
+import Link from "next/link";
 import { Header } from "views/Header";
-import { SignInForm } from "views/SignInForm";
+
+const SignInForm: NextPage = () => {
+  return (
+    <RegisterFormContainer component="form">
+      <Typography variant="h4" align="center" sx={{ mb: "1rem" }}>
+        Sign in
+      </Typography>
+      <Typography variant="subtitle1" align="center" sx={{ mb: "1rem" }}>
+        Join the hot topic and have fun!
+      </Typography>
+      <TextInput
+        icon={<AccountCircle sx={{ mr: 1, mb: "1rem" }} />}
+        placeholder="account"
+        sx={{ mb: "1rem" }}
+      />
+      <TextInput
+        icon={<LockIcon sx={{ mr: 1 }} />}
+        placeholder="password"
+        type="password"
+      />
+      <HorizontalEnd>
+        <Button>Forgot password?</Button>
+      </HorizontalEnd>
+      <Button variant="contained" sx={{ width: "100%", mb: "1rem" }}>
+        Sign in
+      </Button>
+      <Divider />
+      Need an account?
+      <Button>
+        <Link href="/signup">Sign up</Link>
+      </Button>
+    </RegisterFormContainer>
+  );
+};
 
 const SignIn: NextPage = () => {
   return (
