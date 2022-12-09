aaa0657bf8ed90620daa154e51d25cafd00396e6
ek2061 <jackey860517@gmail.com>
2022-12-09 15:08:51 +0800

    feat: add credentials to save session

--- store/api.ts
+++ store/api.ts
@@ -2,6 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
   baseUrl: process.env.YUCHI_API,
+  credentials: "include",
 });
 
 export const apiSlice = createApi({
