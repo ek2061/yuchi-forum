eb455bfa533f70b3d31dd7ea54365cd02c69e0fc
ek2061 <jackey860517@gmail.com>
2023-01-10 13:14:03 +0800

    refactor: extract only required variables from credentials

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,17 +2,22 @@ import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 
 export default NextAuth({
+  session: { strategy: "jwt" },
   providers: [
     CredentialsProvider({
-      name: "credentials",
+      type: "credentials",
       credentials: {},
       async authorize(credentials) {
+        const { account, password } = credentials as {
+          account: string;
+          password: string;
+        };
         const response = await fetch(
           `${process.env.NEXT_PUBLIC_YUCHI_API}/user/login`,
           {
             method: "POST",
             headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(credentials),
+            body: JSON.stringify({ account, password }),
           }
         );
         if (response.status >= 400) return null;
